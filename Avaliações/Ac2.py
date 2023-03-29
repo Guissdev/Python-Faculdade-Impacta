d300a84e3c9879a4d176bb2c3ac145ff953dc281
Guilherme Santos <guiss.devs@gmail.com>
2023-03-29 19:54:55 -0300

    Deu trabalho mas foi, parte 2 ac2

--- Avaliações/Ac2.py
+++ Avaliações/Ac2.py
@@ -23,4 +23,22 @@ while True:
         print('O numero digitado não é maior ou igual a dois')
 
 
-#
+#Professor, mas é só 0.5
+while True:
+    trabalhos = float(input('Entre com a nota dos trabalhos de 0 a 10: '))
+    prova = float(input('Entre com a nota da prova de 0 a 10: '))
+
+    if 0 <= trabalhos <= 10 and 0 <= prova <= 10:
+        media = (trabalhos + prova) / 2
+        if media >= 6:
+            print('aprovado')
+            break
+        else:
+            if 2 > trabalhos:
+                print('reprovado')
+                break
+            else:
+                print('talvez com a sub')
+                break
+    else:
+        print('Valor inválido')
